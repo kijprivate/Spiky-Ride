d1f3d51499569ebfa84198266ba75d7a76f83701
DESKTOP-6JOJD7C\Kuba <brodaczz1@gmail.com>
2018-12-09 13:48:28 +0100

    Created project, simple prototype

---
+++ Assets/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 8f;
+
+    [SerializeField]
+    float offsetZAxis = 2f;
+
+    Rigidbody2D rigidBody;
+    Vector3 direction;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        direction = Vector3.right;
+        rigidBody.velocity = speed * (direction + Vector3.up) + new Vector3(0f,0f,offsetZAxis);
+        direction = -direction;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rigidBody.velocity = speed * (direction + Vector3.up) + new Vector3(0f, 0f, offsetZAxis);
+        if(speed>8f)
+        { speed -= 1f; }
+        direction = -direction;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rigidBody.velocity = -20f * direction;
+            speed += 1f;
+        }
+    }
+}
---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    void Update()
+    {
+        this.transform.position = new Vector3(0f, player.transform.position.y,0f);
+    }
+}
